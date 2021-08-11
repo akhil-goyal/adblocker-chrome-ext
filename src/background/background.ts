e55ad6e2b54006a29e7caa62067bac94c533eda5
Akhil Goyal <akhilgoyal@akhils-mbp.lan>
2021-08-11 17:01:52 -0400

    Added permissions to block the ads before web Request.

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,5 +1,11 @@
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  console.log(msg);
-  console.log(sender);
-  sendResponse("From BG Script!!");
-});
+chrome.webRequest.onBeforeRequest.addListener(
+  (details) => {
+    return {
+      cancel: true,
+    };
+  },
+  {
+    urls: ["*://*.googleadservices.com/*", "*://*.tpc.googlesyndication.com/*", "*://*.g.doubleclick.net/*"],
+  },
+  ["blocking"]
+);
